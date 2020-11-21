a57792f4b446f6016b7b492b79721337304e05ca
吴天 <wutian@192.168.0.102>
2020-11-22 00:03:58 +0800

    update

--- Algorithm/List/147-InsertionSortList.py
+++ Algorithm/List/147-InsertionSortList.py
@@ -1,3 +1,4 @@
 #sort a linked list with inserstion sort
 #input 4->2->1->3
 #output 1->2->3->4
+def main(head):
---
+++ Algorithm/List/148-SortList.py
@@ -0,0 +1,5 @@
+#O(1) memory + O(nlogn) time
+#input: 4 -> 2 -> 1 -> 5
+#output: 1 -> 2 -> 4 -> 5
+def main(head):
+    
