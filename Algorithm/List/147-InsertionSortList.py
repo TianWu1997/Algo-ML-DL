3a7e388c479aa2c79cf8155a2ba272618459bede
吴天 <wutian@wutians-MacBook-Air.local>
2020-12-25 23:45:29 +0800

    delted

--- Algorithm/List/147-InsertionSortList.py
+++
@@ -1,4 +0,0 @@
-#sort a linked list with inserstion sort
-#input 4->2->1->3
-#output 1->2->3->4
-def main(head):
--- Algorithm/List/148-SortList.py
+++
@@ -1,5 +0,0 @@
-#O(1) memory + O(nlogn) time
-#input: 4 -> 2 -> 1 -> 5
-#output: 1 -> 2 -> 4 -> 5
-def main(head):
-    
